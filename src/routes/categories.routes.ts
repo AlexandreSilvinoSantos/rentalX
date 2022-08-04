b3ead68bf5be1c8301f9b7ef3993dbc6a68c95d0
AlexandreSilvinoSantos <assantos_2@yahoo.com.br>
2022-08-03 21:26:53 -0300

    Ajustando padrões de objeto
    Ajustando padrões de objeto

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,21 @@
-import { request, Router } from "express";  //o ruter nos ajuda a manipular as rotas de forma modular
-import { v4 as uuidV4 } from "uuid"
-import { Category } from "../model/category";
-const categoriesRoutes = Router ();
+import { Router } from "express";  //o ruter nos ajuda a manipular as rotas de forma modular
 
-const categories: Category[] = [];
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router ();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category: Category = {
-        name,
-        description,
-        id: uuidV4(),
-        created_at: new Date(),
-    };
-
-    categories.push(category);
+    categoriesRepository.create({ name, description });
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const lista = categoriesRepository.list();
+    return response.json(lista);
 });
 
 export { categoriesRoutes };
